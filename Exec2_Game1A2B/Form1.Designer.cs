62bc3d78c9c958af86dbd3b2e9f6d332d9f93e55
陳品淳 <alice21034242@gmail.com>
2022-10-31 20:24:30 +0800

    加入專案檔案。

---
+++ Exec2_Game1A2B/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace Exec2_Game1A2B
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.CalcButton = new System.Windows.Forms.Button();
+			this.InputTextBox = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.NewButton = new System.Windows.Forms.Button();
+			this.ShowTextBox = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// CalcButton
+			// 
+			this.CalcButton.Location = new System.Drawing.Point(272, 107);
+			this.CalcButton.Name = "CalcButton";
+			this.CalcButton.Size = new System.Drawing.Size(94, 29);
+			this.CalcButton.TabIndex = 0;
+			this.CalcButton.Text = "Star";
+			this.CalcButton.UseVisualStyleBackColor = true;
+			this.CalcButton.Click += new System.EventHandler(this.CalcButton_Click);
+			// 
+			// InputTextBox
+			// 
+			this.InputTextBox.Location = new System.Drawing.Point(125, 107);
+			this.InputTextBox.Name = "InputTextBox";
+			this.InputTextBox.Size = new System.Drawing.Size(125, 27);
+			this.InputTextBox.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(55, 107);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(42, 19);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "輸入:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(55, 38);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(108, 19);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "請輸入4個數字";
+			// 
+			// NewButton
+			// 
+			this.NewButton.Location = new System.Drawing.Point(272, 33);
+			this.NewButton.Name = "NewButton";
+			this.NewButton.Size = new System.Drawing.Size(94, 29);
+			this.NewButton.TabIndex = 0;
+			this.NewButton.Text = "新遊戲";
+			this.NewButton.UseVisualStyleBackColor = true;
+			this.NewButton.Click += new System.EventHandler(this.NewButton_Click);
+			// 
+			// ShowTextBox
+			// 
+			this.ShowTextBox.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.ShowTextBox.ForeColor = System.Drawing.SystemColors.ScrollBar;
+			this.ShowTextBox.Location = new System.Drawing.Point(55, 176);
+			this.ShowTextBox.Name = "ShowTextBox";
+			this.ShowTextBox.Size = new System.Drawing.Size(215, 27);
+			this.ShowTextBox.TabIndex = 1;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(419, 461);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.ShowTextBox);
+			this.Controls.Add(this.InputTextBox);
+			this.Controls.Add(this.NewButton);
+			this.Controls.Add(this.CalcButton);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button CalcButton;
+		private System.Windows.Forms.TextBox InputTextBox;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button NewButton;
+		private System.Windows.Forms.TextBox ShowTextBox;
+	}
+}
+
---
+++ Exec2_Game1A2B/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exec2_Game1A2B
+{
+	public partial class Form1 : Form
+	{
+		
+
+		public Form1()
+		{
+			InitializeComponent();
+			
+
+		}
+		private void NewButton_Click(object sender, EventArgs e)
+		{
+			var game = new ABNumber();
+			ShowTextBox.Text = game.NewGame();
+		}
+
+		private void CalcButton_Click(object sender, EventArgs e)
+		{
+
+			int a = 0, b = 0;
+			string gnum = InputTextBox.Text;
+			string answer = ShowTextBox.Text;
+			
+			char[] arr_gnum = gnum.ToCharArray();
+			
+			char[] arr_answer = answer.ToCharArray();
+			for (int k = 0; k < 4; k++)
+			{
+				for (int p = 0; p < 4; p++)
+				{
+					if (arr_gnum[k] == arr_answer[p])
+					{
+						if (k == p)
+						{
+							a++;
+						}
+						else if (k != p)
+						{
+							b++;
+						}
+
+					}
+
+				}
+			}
+			MessageBox.Show(a.ToString()+"A"+b.ToString()+"B");
+		}
+
+	}
+	
+	public class ABNumber
+		{
+			private string answer { get; set; }
+			private bool check_str { get; set; }//布林宣告check_str，判斷數字是否重複
+			public string NewGame() 
+			{
+				check_str = false;//預設false
+				var random = new Random();
+				answer = random.Next(1000, 9999).ToString();//亂數1000-9999
+				check_str = Check_num(answer);//檢查亂數4碼是否重複
+				while (check_str == false)//當有重複時，check_str 會為false，則需重取亂數並檢查，直到沒重複為止
+				{
+					answer = random.Next(1000, 9999).ToString();
+					check_str = Check_num(answer);
+				}
+				return answer;
+			}
+			public bool Check_num(string str)
+			{
+				char[] arr_str = str.ToCharArray();
+				if (arr_str[0] == arr_str[1] || arr_str[0] == arr_str[2] || arr_str[0] == arr_str[3] || arr_str[1] == arr_str[2] || arr_str[1] == arr_str[3] || arr_str[2] == arr_str[3])
+				{
+					return false;
+				}
+				else 
+				{
+					return true;
+				}
+			}
+		}
+	}
